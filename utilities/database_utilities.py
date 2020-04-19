bb51f9851723ce6d5469b04e4243a115a6ec7d1e
ruizsandovals <sergio.ruizsandoval@gmail.com>
2020-04-19 13:48:00 -0400

    Initial Commit

---
+++ utilities/database_utilities.py
@@ -0,0 +1,8 @@
+def create_database(database_path: str):
+    # TODO: generate the database
+    pass
+
+
+def save_word_to_database(database_path: str, words_list: list):
+    # TODO: save the words to the database
+    pass
